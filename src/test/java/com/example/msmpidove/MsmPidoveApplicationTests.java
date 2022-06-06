1ae6572c6384a575ad6666a0c5c64d41852d334d
Kaio <kaio.giovanni3@gmail.com>
2022-06-06 19:11:30 -0300

    Initial commit

---
+++ src/test/java/com/example/msmpidove/MsmPidoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msmpidove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsmPidoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
